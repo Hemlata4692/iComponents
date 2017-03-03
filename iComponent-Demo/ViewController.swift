7e5aec62182a63757d368ddbf561e1c77e9ec34a
Tak Rahul <rahul.tak@ranosys.com>
2017-03-03 09:27:32 +0530

    Arrange components in table view.

--- iComponent-Demo/ViewController.swift
+++ iComponent-Demo/ViewController.swift
@@ -7,18 +7,63 @@
 //
 
 import UIKit
-import iComponents
 
-class ViewController: UIViewController {
+enum componentViews {
+    case GestureAnimationView
+    case ImageCropperView
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Constans
+    //This will use for navigate on the specific component
+    let GestureAnimation = "GestureAnimation"   // 0 row
+    let imageCropper = "ImageCropper"           // 1 row
+    var componentArray = Array<String>()
+    var viewDict : Dictionary = [String:UIViewController]()
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        componentArray = [GestureAnimation,imageCropper]
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return componentArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = componentArray[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedView = componentArray[indexPath.row]
+        navigateViewController(controller: selectedView)
+
+    }
+    
+    func navigateViewController(controller: String)  {
+        switch controller {
+        case GestureAnimation:
+            let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: controller) as! GestureTableViewController
+            self.navigationController?.pushViewController(nextViewController, animated: true)
+            
+        
+        default:
+            print("bye")
+        }
     }
 }
 
