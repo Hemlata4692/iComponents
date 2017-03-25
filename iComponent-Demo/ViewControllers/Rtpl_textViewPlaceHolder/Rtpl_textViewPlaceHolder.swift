b870493c5a1025d2beb6d75d301395d0ee87e245
Roshan Singh Bisht <your@email.com>
2017-03-25 12:29:21 +0530

    added use guide lines for UItext View

--- iComponent-Demo/ViewControllers/Rtpl_textViewPlaceHolder/Rtpl_textViewPlaceHolder.swift
+++ iComponent-Demo/ViewControllers/Rtpl_textViewPlaceHolder/Rtpl_textViewPlaceHolder.swift
@@ -10,5 +10,10 @@ import Foundation
 import iComponents
 
 class ViewController: UIViewController {
-    
+    override func viewDidLoad() {
+        
+    // To use this class just give the class f text view to Rtpl_textViewPlaceholderLabel Class and all set u can control its properties dynamically anytime.
+        
+        
+    }
 }
