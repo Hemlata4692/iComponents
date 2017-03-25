627ceefff641684a6aeeef0f786751df24fded29
Roshan Singh Bisht <your@email.com>
2017-03-25 12:23:27 +0530

    Code Done for Dynamic Text Via Place Holder

---
+++ iComponent-Demo/ViewControllers/Rtpl_textViewPlaceHolder/Rtpl_textViewPlaceHolder.swift
@@ -0,0 +1,14 @@
+//
+//  Rtpl_textViewPlaceHolder.swift
+//  iComponents
+//
+//  Created by Roshan Singh Bisht on 25/03/17.
+//  Copyright © 2017 Ranosys. All rights reserved.
+//
+
+import Foundation
+import iComponents
+
+class ViewController: UIViewController {
+    
+}
---
+++ iComponents/Rtpl_textViewPlaceHolderClass.swift
@@ -0,0 +1,142 @@
+//
+//  Rtpl_textViewPlaceHolderClass.swift
+//  iComponents
+//
+//  Created by Roshan Singh Bisht on 25/03/17.
+//  Copyright © 2017 Ranosys. All rights reserved.
+//
+
+import Foundation
+public class Rtpl_textViewPlaceHolderClass: UITextView {
+    
+    
+    // taking a default Placeholder Color for placeholder label
+    private struct Constants {
+        static let defaultiOSPlaceholderColor = UIColor(red: 0.0, green: 0.0, blue: 0.0980392, alpha: 0.22)
+    }
+    open let placeholderLabel: UILabel = UILabel()
+    
+    private var placeholderLabelConstraints = [NSLayoutConstraint]()
+    
+    
+    // Getting the run time values which user has set for text view placeholder value's
+    @IBInspectable open var placeholder: String = "" {
+        didSet {
+            placeholderLabel.text = placeholder
+        }
+    }
+    
+    @IBInspectable open var placeholderColor: UIColor = Rtpl_textViewPlaceHolderClass.Constants.defaultiOSPlaceholderColor {
+        didSet {
+            placeholderLabel.textColor = placeholderColor
+        }
+    }
+    
+    override open var font: UIFont! {
+        didSet {
+            if placeholderFont == nil {
+                placeholderLabel.font = font
+            }
+        }
+    }
+    
+    open var placeholderFont: UIFont? {
+        didSet {
+            let font = (placeholderFont != nil) ? placeholderFont : self.font
+            placeholderLabel.font = font
+        }
+    }
+    
+    override open var textAlignment: NSTextAlignment {
+        didSet {
+            placeholderLabel.textAlignment = textAlignment
+        }
+    }
+    
+    // Method to trigger if the text of textview as been changed
+    override open var text: String! {
+        didSet {
+            textDidChange()
+        }
+    }
+    
+    // Method for attributed Text did change
+    override open var attributedText: NSAttributedString! {
+        didSet {
+            textDidChange()
+        }
+    }
+    
+    // Setting the Edge Constants for text View if Applied
+    override open var textContainerInset: UIEdgeInsets {
+        didSet {
+            updateConstraintsForPlaceholderLabel()
+        }
+    }
+    
+    override public init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        commonInit()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(textDidChange),
+                                               name: NSNotification.Name.UITextViewTextDidChange,
+                                               object: nil)
+        
+        placeholderLabel.font = font
+        placeholderLabel.textColor = placeholderColor
+        placeholderLabel.textAlignment = textAlignment
+        placeholderLabel.text = placeholder
+        placeholderLabel.numberOfLines = 0
+        placeholderLabel.backgroundColor = UIColor.clear
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(placeholderLabel)
+        updateConstraintsForPlaceholderLabel()
+    }
+    
+    private func updateConstraintsForPlaceholderLabel() {
+        var newConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|-(\(textContainerInset.left + textContainer.lineFragmentPadding))-[placeholder]",
+            options: [],
+            metrics: nil,
+            views: ["placeholder": placeholderLabel])
+        newConstraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-(\(textContainerInset.top))-[placeholder]",
+            options: [],
+            metrics: nil,
+            views: ["placeholder": placeholderLabel])
+        newConstraints.append(NSLayoutConstraint(
+            item: placeholderLabel,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: .width,
+            multiplier: 1.0,
+            constant: -(textContainerInset.left + textContainerInset.right + textContainer.lineFragmentPadding * 2.0)
+        ))
+        removeConstraints(placeholderLabelConstraints)
+        addConstraints(newConstraints)
+        placeholderLabelConstraints = newConstraints
+    }
+    
+    @objc private func textDidChange() {
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        placeholderLabel.preferredMaxLayoutWidth = textContainer.size.width - textContainer.lineFragmentPadding * 2.0
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: NSNotification.Name.UITextViewTextDidChange,
+                                                  object: nil)
+    }
+    
+}
